e312cfdbba7b6b1e62468b9a15951257399edd51
mehmetbakmaz <115632610+mehmetbakmaz@users.noreply.github.com>
2023-10-30 16:59:55 +0300

    Add files via upload

---
+++ Hayvan Sınıf Diyagramı - Patika/Hayvan Sınıf Diyagramı - Patika/Program.cs
@@ -0,0 +1,11 @@
+﻿using Hayvan_Sınıf_Diyagramı___Patika;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
+
